811931019c42ebc844c80007db5b2cb8d59f23c5
CosignMyCodesign <dillonwilliamsail@gmail.com>
2018-11-01 15:40:30 -0500

    Finished the last practice of Chapter 13

--- powers.js
+++ powers.js
@@ -7,4 +7,42 @@ document.querySelector("#activate-flight").addEventListener("click", () => {
   flightHandlerFunction()
 })
 
+document.querySelector("#activate-mindreading").addEventListener("click", () => {
+  function mindReader () {
+    let mindSect = document.getElementById("mindreading")
+    mindSect.classList.remove("disabled")
+    mindSect.classList.add("enabled")
+  }
+  mindReader()
+})
 
+document.querySelector("#activate-xray").addEventListener("click", () => {
+  function xrayFunc () {
+    let xraySect = document.getElementById("xray")
+    xraySect.classList.remove("disabled")
+    xraySect.classList.add("enabled")
+  }
+  xrayFunc()
+})
+
+document.querySelector("#enable-all").addEventListener("click", () => {
+  function enableFunc() {
+    let allSects = document.querySelectorAll("section")
+    allSects.forEach(sect => {
+      sect.classList.add("enabled")
+      sect.classList.remove("disabled")
+    })
+  }
+  enableFunc()
+})
+
+document.querySelector("#disable-all").addEventListener("click", () => {
+  function disableFunc() {
+    let allSects = document.querySelectorAll("section")
+    allSects.forEach(sect => {
+      sect.classList.add("disabled")
+      sect.classList.remove("enabled")
+    })
+  }
+  disableFunc()
+})
